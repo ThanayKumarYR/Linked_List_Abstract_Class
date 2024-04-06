fe0dc1c9ce67f7bb5c9426aeafd8231370dea840
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-06 13:14:13 +0530

    Implementation of Linked List as an abstract class

---
+++ LinkedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_LinkedList
+{
+    public abstract class LinkedList
+    {
+        public abstract NODE Head{ get; }
+
+        public abstract void Pusp();
+        public abstract void Pop();
+
+        public bool IsEmpty()
+        { 
+            return Head == null;
+        }
+
+        public void Peek()
+        {
+            Console.WriteLine("Element to be poped is " + Head.Info);
+        }
+
+        public void Display()
+        { 
+            if (Head != null)
+            {
+                NODE cur = Head;
+                while (cur != null)
+                {
+                    Console.WriteLine($"{cur.Info} -> ");
+                    cur = cur.Next;
+                }
+            }
+        }
+    }
+}
---
+++ NODE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_LinkedList
+{
+    public class NODE
+    {
+        public int Info { get; set; }
+        public NODE Next { get; set; }
+        public NODE(int data) 
+        {
+            Info = data;    
+            Next = null;
+        }   
+    }
+}
